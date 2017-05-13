e2267cfb69d8addf67fa280ff9143f68ebceb341
Kuba <kuba0506@gmail.com>
2017-05-13 15:51:52 +0200

    [Finish] validateForm all inputs

--- js/form-validator.js
+++ js/form-validator.js
@@ -4,7 +4,22 @@
     const userForm = { };
 
     userForm.validateForm = form => {
+        const result = {};
 
+        const inputs = Array.from(form.querySelectorAll('input'));
+        let isValid = false;
+
+        for (let input of inputs) {
+            if (input.dataset.validation === 'alphabetical') {
+                isValid = new RegExp(input.dataset.pattern, 'i').test(input.value);
+            } else if (input.dataset.validation === 'numeric') {
+                isValid = new RegExp(input.dataset.pattern, 'i').test(input.value);
+            }
+        }
+        
+        result.isValid = isValid;
+
+        return result;
     };
 
     window.userForm = userForm || {};
