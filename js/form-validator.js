405c8b01362e39c39d008c5926d0a9ee80408626
Kuba <kuba0506@gmail.com>
2017-05-13 16:13:25 +0200

    [Finish] validateForm first-name invalid

--- js/form-validator.js
+++ js/form-validator.js
@@ -4,7 +4,12 @@
     const userForm = { };
 
     userForm.validateForm = form => {
-        const result = {};
+        const result = {
+            errors: [],
+            get isValid() {
+                return this.errors.length ? false : true;
+            }
+        };
 
         const inputs = Array.from(form.querySelectorAll('input'));
         let isValid = false;
@@ -12,12 +17,14 @@
         for (let input of inputs) {
             if (input.dataset.validation === 'alphabetical') {
                 isValid = new RegExp(input.dataset.pattern, 'i').test(input.value);
+
+                if (!isValid) {
+                    result.errors.push(new Error(`The ${input.name} ${input.value} is not valid`));
+                }
             } else if (input.dataset.validation === 'numeric') {
                 isValid = new RegExp(input.dataset.pattern, 'i').test(input.value);
             }
         }
-        
-        result.isValid = isValid;
 
         return result;
     };
